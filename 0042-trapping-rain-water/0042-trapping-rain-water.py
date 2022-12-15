744797bd0c0a81740090f90ce9ee6d2d657bf2f6
Nalini-Chennamadhavuni <118326464+Nalini-Chennamadhavuni@users.noreply.github.com>
2022-12-15 14:01:57 -0500

    Time: 103 ms (85.58%), Space: 15 MB (74.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,20 +4,16 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        left, right = 0, len(height)-1
-        left_max, right_max, ans = 0,0,0
-        while left < right:
-            if height[left] < height[right]:
-                if height[left] >= left_max:
-                    left_max = height[left]
-                else:
-                    ans += left_max - height[left]
-                left += 1
-            else:
-                if height[right] >= right_max:
-                    right_max = height[right]
-                else:
-                    ans += right_max - height[right]
-                right -= 1
-        return ans
-                
+        output = 0
+        stack = []
+        
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+                if not stack:
+                    break
+                h = min(height[i], height[stack[-1]]) - height[top]
+                w = i - stack[-1] - 1
+                output += h * w
+            stack.append(i)
+        return output
