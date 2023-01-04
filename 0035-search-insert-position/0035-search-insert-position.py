43090f74245ecc63e992f6131e950068e830c93c
Nalini-Chennamadhavuni <118326464+Nalini-Chennamadhavuni@users.noreply.github.com>
2023-01-04 11:40:26 -0500

    Time: 77 ms (21.89%), Space: 14.4 MB (9.39%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,12 +1,19 @@
 class Solution(object):
+
     def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        if target in nums:
-           return (nums.index(target)) 
+        if nums[-1] < target:
+            return len(nums)
+        elif nums[0] > target:
+            return 0
         else:
-           nums.append(target)
-           return sorted(nums).index(target)
+            return self.search(0, len(nums)-1, nums, target)
+    def search(self, start, end, nums, target):
+        mid = start + (end - start)/2
+        if start == end:
+            return end 
+        if nums[mid] < target:
+            return self.search(mid+1, end, nums, target)
+        elif nums[mid] > target:
+            return self.search(start,mid,nums,target)
+        else:
+            return mid
