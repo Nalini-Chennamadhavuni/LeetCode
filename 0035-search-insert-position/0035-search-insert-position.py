47a52fa8b0247824a01a94e04a0264f3fcc75fdb
Nalini-Chennamadhavuni <118326464+Nalini-Chennamadhavuni@users.noreply.github.com>
2023-01-04 11:23:37 -0500

    Time: 83 ms (11.80%), Space: 13.9 MB (96.71%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,7 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        for i,n in enumerate(nums):
-            if n >= target:
-                return i
-        return len(nums)
+        if target > nums[-1]:
+            return len(nums)
+        elif target < nums[0]:
+            return 0
+        else:
+            for i,n in enumerate(nums):
+                if n >= target:
+                    return i
+            return len(nums)
