e9211862cf2ec6c372b9feb064c39b45c17490f0
Nalini-Chennamadhavuni <118326464+Nalini-Chennamadhavuni@users.noreply.github.com>
2023-01-25 10:33:27 -0500

    Time: 107 ms (33.56%), Space: 14.6 MB (97.19%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -8,4 +8,4 @@ class Solution:
         
        
         lu_sequence = ((c.lower(), c.upper()) for c in s)
-        return list(set([''.join(x) for x in itertools.product(*lu_sequence)]))
+        return list(set(map(''.join, itertools.product(*zip(s.upper(), s.lower())))))
