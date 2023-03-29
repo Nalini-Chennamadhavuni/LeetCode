a1d7e4f74683880d03d0b4258d2f81bbb86385af
Nalini-Chennamadhavuni <118326464+Nalini-Chennamadhavuni@users.noreply.github.com>
2023-03-29 11:12:21 -0400

    Time: 51 ms (79.28%), Space: 13.4 MB (69.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,17 +4,6 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        x = str(x)
-        if not x or x[0] == '-':
+        if x < 0:
             return False
-        if len(x) == 1:
-            return True
-        else:
-            a, b = 0, len(x) - 1
-            while a < b:
-                if x[a] == x[b]:
-                    a += 1
-                    b -= 1
-                else:
-                    return False
-            return True
+        return x == int(str(x)[::-1])
